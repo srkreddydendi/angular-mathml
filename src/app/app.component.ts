f441f0272a77f4b0644a7bfe39f06789485cf43f
srkreddydendi <srkreddydendi@gmail.com>
2020-09-07 16:05:14 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = '<math xmlns = "http://www.w3.org/1998/Math/MathML">         <mrow><msup><mi>a</mi><mn>2</mn></msup>    <mo>+</mo> <msup><mi>b</mi><mn>2</mn></msup> <mo> = </mo>  <msup><mi>c</mi><mn>2</mn></msup </mrow> </math>';
+
+  frac = '<math>    <mfrac bevelled="true">     <mfrac>        <mi> a </mi>     <mi> b </mi>     </mfrac>     <mfrac>        <mi> c </mi>        <mi> d </mi>     </mfrac>  </mfrac></math>'
 }
